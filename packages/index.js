d91171c7d6cc90630fd83f0ab8d3c00ceec53536
Nnamani Ugochukwu Frederick <royal.ugoh@gmail.com>
2023-08-29 13:08:45 +0100

    Create a simple app to evaluate how open source libraries work

---
+++ packages/index.js
@@ -0,0 +1,7 @@
+//Simple example to text the power of open souurce libraries
+
+const {mean, pow, median} = require('mathjs')
+
+console.log(mean([2, 4, 6, 8, 12, 45]));
+console.log(pow(5,5));
+console.log(median([22, 44, 13, 15, 4]));
