bbb770ee4c20fa42a6185d67296359f72dfb3079
Nnamani Ugochukwu Frederick <royal.ugoh@gmail.com>
2023-08-31 13:15:56 +0100

    Work on reading folders

--- fs/folders.js
+++ fs/folders.js
@@ -4,12 +4,19 @@ const {join} = require('path');
 const log = console.log
 const folderPath = join(__dirname, 'folderToWorkWith');
 
-mkdir(folderPath, err => {
+// mkdir(folderPath, err => {
+//     if (err) {
+//         log(err);
+//         return;
+//     }
+//     log('Folder Created successfully.')
+// })
+
+readdir(folderPath, 'utf8', (err, contents) => {
     if (err) {
         log(err);
         return;
     }
-    log('Folder Created successfully.')
+    log(`File Contents read successfully \n ${contents}`);
 })
-
 
